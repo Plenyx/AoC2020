200a18ea8a4dc23e97d9360373114f02356767f6
Plenyx <1507236+Plenyx@users.noreply.github.com>
2020-12-02 18:30:43 +0100

    Added class library for input loading & base for Day2

---
+++ AoCInputLoader/InputLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AoCInputLoader
+{
+    public static class InputLoader
+    {
+        public static List<string> GetInputAsList(string filename)
+        {
+            var generatedList = new List<string>();
+            if (File.Exists(filename))
+            {
+                try
+                {
+                    using var reader = new StreamReader(filename);
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        generatedList.Add(line);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error encountered: {e.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"File '{filename}' does not exist");
+            }
+            return generatedList;
+        }
+
+        public static List<int> GetInputAsNumberList(string filename) => GetInputAsList(filename).Cast<int>().ToList();
+    }
+}
