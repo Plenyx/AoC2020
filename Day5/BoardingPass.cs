f55e5a2708f66ea07b8339511031adc919a69c97
Plenyx <1507236+Plenyx@users.noreply.github.com>
2020-12-05 14:17:06 +0100

    Day5 solved

---
+++ Day5/BoardingPass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Day5
+{
+    public class BoardingPass
+    {
+        public int Row { get; set; }
+
+        public int Column { get; set; }
+
+        public int SeatID
+        {
+            get
+            {
+                return Row * 8 + Column;
+            }
+        }
+
+        public static BoardingPass ParseInput(string input)
+        {
+            // row
+            int rowBelow = 0;
+            int rowUpper = 127;
+            foreach (var character in input[0..^3])
+            {
+                switch(character)
+                {
+                    case 'F':
+                        rowUpper -= (rowUpper - rowBelow + 1) / 2;
+                        break;
+                    case 'B':
+                        rowBelow += (rowUpper - rowBelow + 1) / 2;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+            int columnBelow = 0;
+            int columnUpper = 7;
+            foreach (var character in input[7..])
+            {
+                switch (character)
+                {
+                    case 'L':
+                        columnUpper -= (columnUpper - columnBelow + 1) / 2;
+                        break;
+                    case 'R':
+                        columnBelow += (columnUpper - columnBelow + 1) / 2;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+            return new BoardingPass() { Row = rowBelow, Column = columnBelow };
+        }
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using AoCHelperClasses;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputList = (args.Count() > 0) ? InputLoader.GetInputAsList(args[0]) : InputLoader.GetInputAsList("input.txt");
+            var boardingPasses = inputList.Select(anon => BoardingPass.ParseInput(anon)).ToList();
+            var highestID = boardingPasses.Max(anon => anon.SeatID);
+            Console.WriteLine($"The highest seat ID (part 1): {highestID}");
+            var emptySeats = new List<BoardingPass>();
+            for(var i = 0; i < 128; i++)
+            {
+                for(var j = 0; j < 8; j++)
+                {
+                    var doesExistInPasses = boardingPasses
+                        .Where(anon => anon.SeatID.Equals(i * 8 + j))
+                        .Count();
+                    if (doesExistInPasses == 0)
+                    {
+                        emptySeats.Add(new BoardingPass() { Row = i, Column = j });
+                    }
+                }
+            }
+            foreach(var mySeat in emptySeats)
+            {
+                var leftSeatExists = boardingPasses
+                    .Where(anon => (mySeat.SeatID - 1).Equals(anon.SeatID))
+                    .Count() != 0;
+                var rightSeatExists = boardingPasses
+                    .Where(anon => (mySeat.SeatID + 1).Equals(anon.SeatID))
+                    .Count() != 0;
+                if(leftSeatExists && rightSeatExists)
+                {
+                    Console.WriteLine($"My seat is ID: {mySeat.SeatID}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
